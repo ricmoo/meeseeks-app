dae009983d0456506cb4f02cb0f8a00d7da2085d
Richard Moore <github@ricmoo.com>
2018-11-28 18:30:06 -0500

    Added serve and flatten.

--- bin/meeseeks.js
+++ bin/meeseeks.js
@@ -3,8 +3,12 @@
 'use strict';
 
 const fs = require('fs');
+const http = require('http');
+const https = require('https');
 const inherits = require('util').inherits;
+const parseUrl = require('url').parse;
 
+const UglifyCSS = require("uglifycss");
 const UglifyJS = require("uglify-es");
 
 const cli = require('ethers-cli/lib/cli');
@@ -14,6 +18,8 @@ const ipfs = (function() {
     return ipfsAPI('ipfs.infura.io', '5001', { protocol: 'https' });
 })();
 const basex = require('base-x');
+
+const version = require('../package.json').version;
 
 const base32 = new basex("abcdefghijklmnopqrstuvwxyz234567");
 const base58 = new basex("123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz");
@@ -29,11 +35,15 @@ function getMimeType(filename) {
     let ext = filename.toLowerCase().match(/\.([^.]*)$/);
     if (!ext) { return 'application/octet-stream'; }
     switch (ext[1]) {
+        case 'css':
+            return 'text/css';
         case 'gif':
             return 'image/gif';
         case 'jpeg':
         case 'jpg':
             return 'image/jpeg';
+        case 'js':
+            return 'application/javascript';
         case 'png':
             return 'image/png';
         default:
@@ -41,6 +51,73 @@ function getMimeType(filename) {
     }
     return 'application/octet-stream';
 }
+
+function flatten(filename) {
+    let content = fs.readFileSync(filename).toString();
+
+    function replCss(all, params0, params1) {
+        let href = [];
+        function repl(all, filename) {
+            // Remote CSS
+            if (filename.indexOf('://') >= 0) { return all; }
+
+            href.push(filename);
+            return '';
+        }
+        params0 = params0.replace(/href\s*=\s*"([^"]*)"/i, repl).trim();
+        params1 = params1.replace(/href\s*=\s*"([^"]*)"/i, repl).trim();
+
+        if (href.length === 0) { return all; }
+        // @TODO: Complain if more than 1 href?
+
+        return '<style type="text/css">' + UglifyCSS.processString(fs.readFileSync(href[0]).toString()) + "</style>";
+    }
+
+    function replImg(all, params) {
+        function repl(all, filename) {
+            // Remote image
+            if (filename.indexOf('://') >= 0) { return all; }
+
+            // Local image; inline it
+            let content = fs.readFileSync(filename).toString('base64');
+            return 'src="data:' + getMimeType(filename) + ';base64,' + content + '"';
+        }
+        params = params.replace(/src\s*=\s*"([^"]*)"/i, repl).trim();
+        return ['<img', (params.length ? ' ': ''), params, '>'].join('');
+    }
+
+    function replJs(all, params) {
+        let src = null;
+        function repl(all, filename) {
+            // Remote JavaScript
+            if (filename.indexOf('://') >= 0) { return all; }
+
+            src = filename;
+            return '';
+        }
+        params = params.replace(/src\s*=\s*"([^"]*)"/i, repl).trim();
+
+        // No source, return unmodified
+        if (src == null) { return all; }
+
+        let content = fs.readFileSync(src).toString().trim();
+        content = UglifyJS.minify(content);
+
+        return [ '<script', (params.length ? ' ': ''), params, '>', content.code, '</script>'].join('');
+    }
+
+    // <link rel="stylesheet" href="styles.css">
+    content = content.replace(/<\s*link([^>]+)rel\s*=\s*"stylesheet"([^>]*)>/ig, replCss);
+
+    // <img src="blah" />
+    content = content.replace(/<\s*img([^>]*)>/ig, replImg);
+
+    // <script src="blah"></script>
+    content = content.replace(/<\s*script([^>]*)>(\s*<\s*\/\s*script\s*>)/ig, replJs);
+
+    return content;
+}
+
 
 let plugins = {};
 
@@ -50,15 +127,25 @@ function PublishPlugin() {
 inherits(PublishPlugin, cli.Plugin);
 
 PublishPlugin.prototype.help = "FILENAME";
+PublishPlugin.prototype.options = {
+    "flatten": "Flatten the file before publishing (experimental)"
+};
+options.flatten = false;
 PublishPlugin.prototype.prepare = function(opts) {
     if (opts.args.length !== 2) {
         throw new Error('publish requires FILENAME');
     }
     this.filename = opts.args[1];
+    this.flatten = opts.options.flatten;
     return Promise.resolve();
 }
 PublishPlugin.prototype.run = function() {
-    let content = fs.readFileSync(this.filename);
+    let content = null;
+    if (this.flatten) {
+        content = Buffer.from(flatten(this.filename));
+    } else {
+        content = fs.readFileSync(this.filename);
+    }
     console.log('Publishing:', this.filename, '(' + content.length + ' bytes)');
     return ipfs.files.add(content).then((results) => {
         console.log('  Hash: ' + results[0].hash);
@@ -128,37 +215,97 @@ FlattenPlugin.prototype.prepare = function(opts) {
     return Promise.resolve();
 }
 FlattenPlugin.prototype.run = function() {
-    let content = fs.readFileSync(this.filename).toString();
-    function replImg(all, params) {
-        function repl(all, filename) {
-            let content = fs.readFileSync(filename).toString('base64');
-            return 'src="data:' + getMimeType(filename) + ';base64,' + content + '"';
-        }
-        params = params.replace(/src\s*=\s*"([^"]*)"/i, repl).trim();
-        console.log(JSON.stringify(params));
-        return ['<img', (params.length ? ' ': ''), params, '>'].join('');
-    }
-    function replJs(all, params) {
-        let src = null;
-        function repl(all, filename) {
-            src = filename;
-            return '';
-        }
-        params = params.replace(/src\s*=\s*"([^"]*)"/i, repl).trim();
-
-        let content = fs.readFileSync(src).toString().trim();
-        content = UglifyJS.minify(content);
-
-        return [ '<script', (params.length ? ' ': ''), params, '>', content.code, '</script>'].join('');
-    }
-
-    content = content.replace(/<\s*img([^>]*)>/ig, replImg);
-    content = content.replace(/<\s*script([^>]*)>(\s*<\s*\/\s*script\s*>)/ig, replJs);
-
+    let content = flatten(this.filename);
     console.log(content);
 
     return Promise.resolve();
 }
 plugins['flatten'] = new FlattenPlugin();
+
+function ServePlugin() {
+    cli.Plugin.call(this);
+}
+inherits(ServePlugin, cli.Plugin);
+
+ServePlugin.prototype.help = "FILENAME";
+ServePlugin.prototype.options = {
+    port: "port to run the server on (default 8000; 8443 for ssl)",
+    key: "path to SSL private key",
+    certificate: "path to SSL certificate",
+};
+options.port = "";
+options.key = "";
+options.certificate = "";
+ServePlugin.prototype.prepare = function(opts) {
+    if (opts.args.length !== 2) {
+        throw new Error('serve requires FILENAME');
+    }
+    this.filename = opts.args[1];
+
+    if (opts.options.key && opts.options.certificate) {
+        let options = {
+           key: fs.readFileSync(opts.options.key).toString(),
+           cert: fs.readFileSync(opts.options.certificate).toString(),
+        };
+        this.createServer = (handler) => {
+            return https.createServer(options, handler);
+        }
+        this.port = parseInt(opts.options.port || "8443")
+    } else {
+        this.createServer = http.createServer;
+        this.port = parseInt(opts.options.port || "8000")
+    }
+
+    return Promise.resolve();
+}
+ServePlugin.prototype.run = function() {
+    let filename = this.filename;
+
+    function handler(request, response) {
+        let isSecure = !!request.connection.ssl;
+
+        let url = parseUrl(request.url);
+
+        function sendContent(content, contentType) {
+            console.log("OK: " + content.length + ' bytes sent');
+            response.writeHead(200, { "Content-Type": contentType });
+            response.write(content);
+            response.end();
+        }
+
+        function sendError(statusCode, reason) {
+            console.log(reason + ": " + url.pathname);
+            response.writeHead(statusCode, reason);
+            response.end();
+        }
+
+        if (request.method === "GET" || request.method === "HEAD") {
+            if (url.pathname !== '/') { return sendError(404, 'NOT FOUND'); }
+            try {
+                return sendContent(flatten(filename), 'text/html');
+            } catch(error) {
+                console.log(error);
+                return sendError(500, 'SERVER ERROR');
+            }
+        } else {
+            return sendError(400, 'Bad Request');
+        }
+    }
+
+    return new Promise((resolve, reject) => {
+        let server = this.createServer(handler);
+        server.listen(this.port, () => {
+            console.log("Listening on port " + this.port + "...");
+        });
+        process.on('SIGINT', function() {
+            console.log("Shutting down...");
+            server.close(() => {
+                resolve();
+            });
+        });
+    });
+}
+plugins['serve'] = new ServePlugin();
+
 
 cli.run(options, plugins)
